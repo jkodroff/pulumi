3cc5c548cc02d52c60442411a6b9d5211c9458ec
Michael Bridgen <mbridgen@pulumi.com>
2022-07-29 10:03:11 +0100

    Generalise git branch test
    In #10118 the git checkout was adapted so it would work with simple
    branch names (i.e., "main" rather than "refs/heads/main"). However, it
    still won't work with a branch that's not the default branch -- this
    test demonstrates so.
    
    Signed-off-by: Michael Bridgen <mbridgen@pulumi.com>

--- sdk/go/auto/git_test.go
+++ sdk/go/auto/git_test.go
@@ -3,32 +3,89 @@ package auto
 import (
 	"context"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
 // This takes the unusual step of testing an unexported func. The rationale is to be able to test
 // git code in isolation; testing the user of the unexported func (NewLocalWorkspace) drags in lots
 // of other factors.
 
-func TestGitShortBranch(t *testing.T) {
+func TestGitBranch(t *testing.T) {
 	t.Parallel()
-	repo := &GitRepo{
-		URL:    "https://github.com/pulumi/test-repo.git",
-		Branch: "master",
-	}
-	tmp, err := os.MkdirTemp("", "pulumi-test")
+
+	// This makes a git repo to clone from, so to avoid relying on something at GitHub that could
+	// change or be inaccessible.
+	tmpDir, err := os.MkdirTemp("", "pulumi-git-test")
 	assert.NoError(t, err)
-	defer os.RemoveAll(tmp)
-	_, err = setupGitRepo(context.TODO(), tmp, repo)
+	assert.True(t, len(tmpDir) > 1)
+	t.Cleanup(func() { os.RemoveAll(tmpDir) })
+	originDir := filepath.Join(tmpDir, "origin")
+
+	origin, err := git.PlainInit(originDir, false)
+	assert.NoError(t, err)
+	w, err := origin.Worktree()
+	assert.NoError(t, err)
+	nondefaultHead, err := w.Commit("nondefault branch", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "testo",
+			Email: "testo@example.com",
+		},
+	})
 	assert.NoError(t, err)
 
-	r, err := git.PlainOpen(tmp)
+	// this sets up two branches: `nondefault` and `default`, with `default` becoming the "default"
+	// branch when cloning, since it's left as the HEAD of the repo.
+	assert.NoError(t, w.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.NewBranchReferenceName("nondefault"),
+		Create: true,
+	}))
+
+	assert.NoError(t, w.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.NewBranchReferenceName("default"),
+		Create: true,
+	}))
+	defaultHead, err := w.Commit("default branch", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "testo",
+			Email: "testo@example.com",
+		},
+	})
 	assert.NoError(t, err)
-	head, err := r.Head()
-	assert.NoError(t, err)
-	assert.Equal(t, plumbing.NewBranchReferenceName("master"), head.Name())
+
+	type testcase struct {
+		branchName   string
+		expectedHead plumbing.Hash
+	}
+
+	for _, tc := range []testcase{
+		{"default", defaultHead},
+		{"nondefault", nondefaultHead},
+	} {
+		tc := tc
+		t.Run(tc.branchName, func(t *testing.T) {
+			t.Parallel()
+			repo := &GitRepo{
+				URL:    originDir,
+				Branch: tc.branchName,
+			}
+
+			tmp, err := os.MkdirTemp(tmpDir, "testcase") // i.e., under the tmp dir from earlier
+			assert.NoError(t, err)
+
+			_, err = setupGitRepo(context.TODO(), tmp, repo)
+			assert.NoError(t, err)
+
+			r, err := git.PlainOpen(tmp)
+			assert.NoError(t, err)
+			head, err := r.Head()
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expectedHead, head.Hash())
+		})
+	}
 }
